c67302084f081d70786e7379783acc6564711830
Marius0509 <marius05alexandru@gmail.com>
2025-05-09 14:34:24 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module inventory {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.controls;
     requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
 
-    opens inventory             to javafx.base, javafx.fxml, javafx.controls, org.junit.platform.commons;
-    opens inventory.model       to javafx.base, javafx.fxml, javafx.controls, org.junit.platform.commons;
-    opens inventory.repository  to javafx.base, javafx.fxml, javafx.controls, org.junit.platform.commons;
-    opens inventory.service     to javafx.base, javafx.fxml, javafx.controls, org.junit.platform.commons;
-    opens inventory.controller  to javafx.base, javafx.fxml, javafx.controls, org.junit.platform.commons;
+    opens inventory             to javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
+    opens inventory.model       to javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
+    opens inventory.repository  to javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
+    opens inventory.service     to javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
+    opens inventory.controller  to javafx.base, javafx.controls, javafx.fxml, javafx.graphics;
 
     exports inventory;
     exports inventory.model;
